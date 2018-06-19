cd083cc4f5bb0fc0bf6ff0ecb0b52fa3480d929b
dylangouzenne <dylan.gouzenne@gmail.com>
2018-06-19 16:06:49 +0200

    exo 6 prix compteur

--- app.js
+++ app.js
@@ -3,18 +3,19 @@ var multiplicateur = $("#multiplicateur");
 var div = $("#display");
 var score = 0;
 var compteur = 0;
+var prix_compteur = 50;
 
 
 
 
-bouton.click(function(){
-	score++; // ou sinon : score = score +1;
+bouton.click(function ajout(){
 	score = score + compteur;
+	score++; // ou sinon : score = score +1;
 	div.html(score);
 });
 
 multiplicateur.click(function incrementer(){
-	score = score + compteur;
+	score = score -50;
 	compteur++;
 	div.html(score)
 });
